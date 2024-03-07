f74f33bff3dc6aaef8831c23ddc0aeabf1ad30bf
Mayowa Ayeni <mayhoral008@gmail.com>
2024-03-07 13:30:38 +0100

    updated url

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ config()
  const discordKey = `${process.env.DISCORD_KEY}`;
  const dburl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xoq86dz.mongodb.net/?retryWrites=true&w=majority`;
  const httpsPort = 5000;
- const url = "http://localhost:5000/verify"
+ const url = "https://verify-wallet-address.onrender.com/verify"
 
  module.exports = {discordKey, dburl, httpsPort, url}
