1d3b97ceac78f8c0c83d7a728077fee1c63f1b47
Mayowa Ayeni <mayhoral008@gmail.com>
2024-03-08 05:34:46 +0100

    tried logging some data to help debug the issue in production

--- app.js
+++ app.js
@@ -34,7 +34,10 @@ credentials = {};
 // }
 const addWalletAddress = async (discordUid, address, username) => {
   const doc = await WalletsModel.findOne({ discordUid });
-  if (true) {
+  console.log(doc?._doc.addresses.includes(address));
+  if (doc?._doc.addresses.includes(address)) {
+    console.log("Addresses in document:", doc?._doc.addresses);
+    console.log("Address to check:", address);
     return { success: false, message: 'address verified before.' };
   }else{
     await WalletsModel.findOneAndUpdate(
